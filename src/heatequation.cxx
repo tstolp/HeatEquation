0e4c58d0d24414fa4f7e8ba6d0ea8949a77a94a1
Jens Langerak <J.Langerak@student.tudelft.nl>
2018-02-09 11:24:23 +0100

    Add vector constructor

--- src/heatequation.cxx
+++ src/heatequation.cxx
@@ -2,6 +2,8 @@
 #include <typeinfo>
 #include <math.h>
 #include <cassert>
+#include <initializer_list>
+#include <memory>
 
 /**
  * @brief Test if expected and actual are equal.
@@ -28,6 +30,56 @@ void test(void)
     
     std::cout << "All test passed!" << std::endl;
 }
+
+template<typename T>
+class Vector {
+private:
+    T* data;
+    int size;
+    
+public:
+public:
+    /**
+     * @brief Default constructor for a vector.
+     * @return An vector with size 0.
+     */
+    Vector()
+    : size(0), 
+      data(nullptr)    
+    { }
+    
+    /**
+     * @brief Create a vector of size n
+     * @param n the size of the vector.
+     * @return A vector with size n.
+     */
+    Vector(int n)
+    : data(new T[n]),
+      size(n)
+    { }
+
+    /**
+     * @brief Create a vector and initialize the vector with the values in the list.
+     * @param list the values for the fector.
+     * @return A vector initialized with the values from the list.
+     */
+    Vector(std::initializer_list<T> list)
+    : Vector((int)list.size())
+    {
+        std::uninitialized_copy(list.begin(), list.end(), data);
+    }
+    
+    /**
+     * @brief Destroy the vector.
+     * @return 
+     */
+    ~Vector()
+    {
+        size=0;
+        delete[] data;
+    }
+
+};
 
 int main(){
     test();
