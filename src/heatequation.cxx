b903e1d89dbfdab46879c2eb93344c97307eb34a
Jens Langerak <J.Langerak@student.tudelft.nl>
2018-02-09 11:39:32 +0100

    Start testing vector class

--- src/heatequation.cxx
+++ src/heatequation.cxx
@@ -16,20 +16,6 @@ bool equals(double expected, double actual)
     return fabs(expected - actual) < 0.0001;
 }
 
-/**
- * @brief Test the code.
- */
-void test(void)
-{
-    //test equals
-    assert(equals(2.0, 2.0));
-    assert(!equals(2.0001, 2.00021));
-    
-    
-    
-    
-    std::cout << "All test passed!" << std::endl;
-}
 
 template<typename T>
 class Vector {
@@ -38,7 +24,7 @@ private:
     int size;
     
 public:
-public:
+    
     /**
      * @brief Default constructor for a vector.
      * @return An vector with size 0.
@@ -78,8 +64,48 @@ public:
         size=0;
         delete[] data;
     }
+    
+    template<typename A>
+    bool equals(Vector<A> other)
+    {
+        if (!std::is_same<T, A>::value)
+            return false;
+        
+        if (this->size != other.size)
+            return false;
+        
+        for(int i = 0; i < this->size; i++)
+        {
+            if (this->data[i] != other.data[i])
+                return false;
+        }
+        
+        return true;
+        
+    }
 
 };
+
+/**
+ * @brief Test the code.
+ */
+void test(void)
+{
+    //test equals
+    assert(equals(2.0, 2.0));
+    assert(!equals(2.0001, 2.00021));
+    
+    Vector<double> A;
+    Vector<double> B(0);
+    Vector<double> C({});
+    
+    assert(A.equals(B));
+    assert(A.equals(C));
+    
+    
+    
+    std::cout << "All test passed!" << std::endl;
+}
 
 int main(){
     test();
